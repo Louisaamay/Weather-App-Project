9a1117df23240bf1e2a9cbe4bd26bb15de34ce2e
Louisaamay <105213095+Louisaamay@users.noreply.github.com>
2022-06-30 21:41:50 +1000

    Integrated API to the 5 day forecast

--- src/index.js
+++ src/index.js
@@ -40,6 +40,54 @@ let currentMonth = months[now.getMonth()];
 
 h3.innerHTML = `Last updated: ${currentDay}, ${hours}:${minutes} `;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+            <div class="col-2">
+              <div class="date">${formatDay(forecastDay.dt)}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" 
+              alt="" 
+              width="70px"/>
+              <div class="weather-forecast-temperature">
+               <span class= "weather-forecast-temperature-max">
+              ${Math.round(forecastDay.temp.max)}°|</span>
+              <span class ="weather-forecast-min">
+              ${Math.round(forecastDay.temp.min)}°
+              </span>
+              </div>
+            </div>
+    `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "3dce9b1c66837262a25b3f448d354a76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric  `;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   celciusTemperature = Math.round(response.data.main.temp);
   let icon = document.querySelector("#icon");
@@ -62,6 +110,8 @@ function showTemperature(response) {
   icon.setAttribute("alt", response.data.weather[0].description);
 
   changeVideo(response.data.weather[0].main);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
